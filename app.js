c1ac955cf0e893613a4c7db889bbaa1bde2d1a81
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-01-29 19:50:53 -0500

    edited html and js file

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 'use strict'
 
-let time = prompt('what ')
+let name = prompt('What is your name?');
+let time = prompt('What time is it?');
+let message;
+
+if(time <= 11){
+    message = 'Good Morning!';
+} else if (time <= 18){
+    message = 'Good Afternoon!';
+}  else if(time <= 24){
+    message = "Good Evening!";
+} else {
+    message = "Time does not exist!";
+}
+
+document.write('Hello ' + name + '! ' + message);
+
