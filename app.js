0d78ec78506ded7e13f591df89131a0966288a35
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-02-02 22:59:34 -0500

    added do loops and image

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 'use strict'
 
-let name = prompt('What is your name?');
-let time = prompt('What time is it?');
-let email= prompt( 'what is your email');
+let name = prompt('what is your name?')
+let day = prompt('how is your day going?');
+let time = prompt ('What time is it?');
+let email = prompt ('Sign Up with your email to get a Discount Updates')
 let message;
 
 if(time <= 11){
-    message = 'Good Morning!';
-} else if (time <= 18){
-    message = 'Good Afternoon!';
-}  else if(time <= 24){
-    message = "Good Evening!";
-} else {
-    message = "Time does not exist!";
+    message= 'Have A Good Morning!';
+}
+else if (time <= 18){
+    message = 'Have A Good Afternoon!';
+}
+else if(time <= 24){
+    message = 'Have A Good Evening!';
+}
+else {
+    message = 'Time does not exist';
 }
 
-document.write('Hello ' + name + '! ' + message + email);
+document.write('Hey ' +  name + ' ! ' + message  +   'Welcome to My Clothing Store');
+
+
+
+/////
+let userAnswer = ''
+do{
+   let answer = prompt('guess a number from 1 - 10?');
+   userAnswer = answer 
+
+    if (answer === '5') {
+       console.log('good job');
+      
+    } else {
+         alert('try again');
+    }
+
+} while (userAnswer != '5');
+
+function displayChampagne() {
+    let output= '';
+    let champagne = prompt('scale of 1-5, how many champagne do you want?');
+    for(let i = 0; i < champagne; i++){
+    output= output + "<img class='champagne' src='champagne.png'/>";}
+    return document.write(output);
+
+}
+displayChampagne();
+    
+    
+    
+
 
